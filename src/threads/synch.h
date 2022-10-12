d4a81cdd8f26022ede820d3426f6a9480644978f
ViXbob <vixbob@outlook.com>
2022-10-12 21:05:34 +0800

    add donator_list in struct lock

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -22,6 +22,7 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+    struct list donator_list;
   };
 
 void lock_init (struct lock *);
