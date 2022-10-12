18345719db105cea3bc27fd2e8d94099c51e0c3f
ViXbob <vixbob@outlook.com>
2022-10-12 21:04:32 +0800

    timer_sleep can be called with intr_disable

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -96,7 +96,7 @@ timer_sleep (int64_t ticks)
 
     int64_t start = timer_ticks ();
 
-    ASSERT (intr_get_level () == INTR_ON);
+    // ASSERT (intr_get_level () == INTR_ON);
 
     enum intr_level old_level = intr_disable ();
     thread_block_with_ticks(ticks + start);
