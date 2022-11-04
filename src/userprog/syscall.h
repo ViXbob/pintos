14058dcb88ac3e0fbf0d6e6c9c9d34b6abc4fb33
ViXbob <vixbob@outlook.com>
2022-11-04 22:19:51 +0800

    API for process.c

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -3,4 +3,12 @@
 
 void syscall_init (void);
 
+struct find_thread
+{
+  int tid;
+  struct thread *t;
+};
+
+void find_thread_with_tid (struct thread *t, void *aux);
+
 #endif /* userprog/syscall.h */
