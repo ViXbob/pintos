794a643e74574644c12690a55deb4369936df111
ViXbob <vixbob@outlook.com>
2022-11-05 19:46:33 +0800

    add process_action_fun & struct pcb

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -2,10 +2,24 @@
 #define USERPROG_PROCESS_H
 
 #include "threads/thread.h"
+#include "threads/synch.h"
+#include <list.h>
 
-tid_t process_execute (const char *file_name);
-int process_wait (tid_t);
+int process_execute (const char *file_name);
+int process_wait (int pid);
 void process_exit (void);
 void process_activate (void);
+
+struct process_status
+{
+  int pid;
+  struct thread *t;
+  struct thread *parent_thread;
+  int exit_status;
+  struct lock lock_exit_status;
+  struct list_elem process_elem;
+};
+
+typedef void process_action_func (struct process_status *pcb, void *aux);
 
 #endif /* userprog/process.h */
